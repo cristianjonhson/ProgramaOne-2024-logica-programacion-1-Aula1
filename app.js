f2c411e4270f578d8fc607084d86c6493a4a0c0d
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-06-28 16:06:16 -0400

    version Mejorada del Modulo1-Desafio - hora de practicar

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-// Desafío 1: Muestra una alerta con el mensaje "?Bienvenida y bienvenido a nuestro sitio web!"
-alert("?Bienvenida y bienvenido a nuestro sitio web!");
+// Muestra una alerta con el mensaje "Â¡Bienvenida y bienvenido a nuestro sitio web!"
+alert("Â¡Bienvenida y bienvenido a nuestro sitio web!");
 
-// Desafío 2: Declara una variable llamada nombre y asígnale el valor "Luna".
+// Declara una variable llamada nombre y asÃ­gnale el valor "Luna".
 var nombre = "Luna";
 
-// Desafío 3: Crea una variable llamada edad y asígnale el valor 25.
+// Crea una variable llamada edad y asÃ­gnale el valor 25.
 var edad = 25;
 
-// Desafío 4: Establece una variable numeroDeVentas y asígnale el valor 50.
+// Establece una variable numeroDeVentas y asÃ­gnale el valor 50.
 var numeroDeVentas = 50;
 
-// Desafío 5: Establece una variable saldoDisponible y asígnale el valor 1000.
+// Establece una variable saldoDisponible y asÃ­gnale el valor 1000.
 var saldoDisponible = 1000;
 
-// Desafío 6: Muestra una alerta con el texto "?Error! Completa todos los campos".
-alert("?Error! Completa todos los campos");
-
-// Desafío 7: Declara una variable llamada mensajeDeError y asígnale el valor "?Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError.
-var mensajeDeError = "?Error! Completa todos los campos";
-alert(mensajeDeError);
-
-// Desafío 8: Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
-nombre = prompt("?Cuál es tu nombre?");
-
-// Desafío 9: Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
-edad = prompt("?Cuál es tu edad?");
-
-// Desafío 10: Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "?Puedes obtener tu licencia de conducir!".
-if (edad >= 18) {
-    alert("?Puedes obtener tu licencia de conducir!");
+// Utiliza un prompt para preguntar el nombre del usuario y almacÃ©nalo en la variable nombre.
+nombre = prompt("Â¿CuÃ¡l es tu nombre?");
+if (!nombre) {
+    alert("Â¡Error! Completa todos los campos");
+} else {
+    // Pide al usuario que ingrese su edad usando un prompt y almacÃ©nala en la variable edad.
+    edad = prompt("Â¿CuÃ¡l es tu edad?");
+    if (!edad) {
+        alert("Â¡Error! Completa todos los campos");
+    } else {
+        // Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "Â¡Puedes obtener tu licencia de conducir!".
+        if (edad >= 18) {
+            alert("Â¡Puedes obtener tu licencia de conducir!");
+        }
+    }
 }
